5abb859892a425f5fce025248b29f1ea58df38f7
Burak Asarcıklı <brkasarcikli@outlook.com>
2024-05-12 15:35:38 +0300

    küçük bir değişiklik yaptım

--- src/main/java/org/example/CheckMail.java
+++ src/main/java/org/example/CheckMail.java
@@ -1,13 +1,15 @@
 package org.example;
 
 public class CheckMail {
-    private String email;
-    public boolean isEmailIllegal() {
-        int index = this.email.indexOf("@");
-        if (index == 0 || this.email.length() - index <= 5) {
-            System.out.println("This e-mail is unvalid please use a valid e-mail");
-            return true;
+//    private String email;
+    public boolean isEmailLegal(String email)
+    {
+        int index = email.indexOf("@");
+        if (index == 0 || index == -1) {
+            System.out.println("This e-mail is unvalid please use a valid e-mail");// EXCEPTİON !!!
+            return false;
         }
-        return false;
+        
+        return true;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("MMMMmmmherbaaaa ARkadaşlarrrr ");
+        CheckMail check = new CheckMail();
+        check.isEmailLegal("as@hotmail.com");
     }
 }
