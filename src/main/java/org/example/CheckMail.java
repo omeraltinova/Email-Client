d4663059bb36f8ca7075c5e43d2519cf8f01f4bb
Ömer Faruk Altınova <omerfarukaltinova@gmail.com>
2024-05-14 15:32:29 +0300

    Checkmail + picture

--- src/main/java/org/example/CheckMail.java
+++ src/main/java/org/example/CheckMail.java
@@ -1,15 +1,25 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class CheckMail {
 //    private String email;
     public boolean isEmailLegal(String email)
     {
-        int index = email.indexOf("@");
-        if (index == 0 || index == -1) {
-            System.out.println("This e-mail is unvalid please use a valid e-mail");// EXCEPTİON !!!
-            return false;
+        String patternString = "\\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}\\b";
+
+        Pattern pattern = Pattern.compile(patternString);
+        Matcher matcher = pattern.matcher(email);
+
+        if (matcher.matches()) {
+            System.out.println("Girilen metin bir e-posta adresidir.");
+            return(false);
+
+        } else {
+            System.out.println("Girilen metin bir e-posta adresi değildir.");
+            return(true);
         }
-        
-        return true;
+
     }
 }
