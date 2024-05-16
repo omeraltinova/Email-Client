9a4c7d17f24b75400b41d37c7dd79d5449037752
Ömer Faruk Altınova <omerfarukaltinova@gmail.com>
2024-05-16 16:38:52 +0300

    Main Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,16 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-       // CheckMail check = new CheckMail();
-       // System.out.println(check.isEmailLegal("as@hotmail.com")); //Bende hata gösterdiği için yoruma aldım
+          MailManagement check = new MailManagement();
+          System.out.println(check.isEmailLegal("ruhicenet123_javaproje@outlook.com","RuhiBaba123_Java")); // Mail gönderme sistem
 //        MailManagement mm = new MailManagement();
 //       mm.fetchEmails();
-//        RecieveMail rm = new RecieveMail();
-//        rm.fetchEmails();
+         RecieveMail rm = new RecieveMail();
+         rm.fetchEmails();
 
 //        MailManagement m1 = new MailManagement();
 //
-//        m1.sendPlainTextEmail("ruhicenet123_javaproje@outlook.com","skocraft05@gmail.com","test","test",true);
+//        m1.sendPlainTextEmail("ruhicenet123_javaproje@outlook.com","skocraft05@gmail.com","test","test",true); // ÇALIŞTIRMAYIN!!!
 
         List<Map<String, String>> emails = EmailReader.readEmails("emails");
         //Ana ekranı çağırmak için
