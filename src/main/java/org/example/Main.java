1a29f2eefbb9bf251c577b5a5a944f304b083392
integrateThem <ahmedfaruker19@gmail.com~>
2024-05-23 01:34:20 +0300

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,7 +15,7 @@ public class Main {
 //
 //        for (String fileName : draftFileNames) {
 //            System.out.println(fileName);
-//        }aaa
+//        }aaaaaa
 
         // Hesap seçme ekranını başlatmannbvbn
         List<AccountSelectionScreen.Account> accounts = readAccountsFromFile();
